bdb370cf2f5e3c0a3a406800bccf11b25c0eafe2
Devon McGrath <devon.mcgrath@gmail.com>
2024-02-21 17:18:20 +1300

    Add back inlude and references

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 	return (
 		<>
 			<div className="w-dvh h-dvh p-10">
-				<div className="flex justify-center items-center w-full h-full">
+				<div className="flex justify-center items-center w-full h-full ">
 					<h1>React App</h1>
 				</div>
 			</div>
