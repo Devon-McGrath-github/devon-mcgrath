bc4348908355ce43a08fc942c69a6d57a41ba556
Devon McGrath <devon.mcgrath@gmail.com>
2024-02-22 15:03:06 +1300

    Basic layout w/ placeholders

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,16 @@ import './styles/App.css'
 function App() {
 	return (
 		<>
-			<div className="w-dvh h-dvh p-10">
-				<div className="flex justify-center items-center w-full h-full ">
-					<h1>React App</h1>
+			<div className="w-dvh h-dvh p-16">
+				<div className="grid grid-rows-2 grid-cols-2 w-full h-full border-4 rounded-sm ">
+					<h1>Devon McGrath</h1>
+
+					<div className="flex flex-row gap-4 row-start-2 col-start-2 items-end justify-end">
+						<p>Portfolio</p>
+						<p>Github</p>
+						<p>Linkedin</p>
+						<p>contact</p>
+					</div>
 				</div>
 			</div>
 		</>
