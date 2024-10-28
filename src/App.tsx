b348b2e403a9befd5e99e61dec3d142337b03e13
Devon McGrath <devon.mcgrath@gmail.com>
2024-10-29 11:43:43 +1300

    Update layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,30 @@ import './styles/App.css'
 function App() {
 	return (
 		<>
-			<div className="w-dvh h-dvh p-16">
-				<div className="grid grid-rows-2 grid-cols-2 w-full h-full border-4 rounded-[2px] ">
-					<h1>Devon McGrath</h1>
-
-					<div className="flex flex-row gap-4 row-start-2 col-start-2 items-end justify-end">
-						{/* <Button variant={'ghost'}>
-							<a href="" >Portfolio</a>
-						</Button> */}
-						<Button variant={'ghost'}>
-							<a href="https://github.com/Devon-McGrath-github" target="_blank">
-								Github
-							</a>
-						</Button>
-						<Button variant={'ghost'}>
-							<a href="https://www.linkedin.com/in/devon-mcgrath-17462558/" target="_blank">
-								Linkedin
-							</a>
-						</Button>
-						<Button variant={'ghost'}>
-							<a href="">Contact</a>
-						</Button>
-					</div>
+			<main className="flex flex-col h-full">
+				<div className="flex flex-row gap-4 row-start-2 col-start-2 items-end justify-center">
+					<Button variant={'ghost'}>
+						<a href="https://github.com/Devon-McGrath-github" target="_blank">
+							Github
+						</a>
+					</Button>
+					<Button variant={'ghost'}>
+						<a href="https://www.linkedin.com/in/devon-mcgrath-17462558/" target="_blank">
+							Linkedin
+						</a>
+					</Button>
+					<Button variant={'ghost'}>
+						<a href="">Contact</a>
+					</Button>
 				</div>
-			</div>
+				<div className="flex flex-col flex-[1_0_auto]">
+					<h1>Devon McGrath</h1>
+				</div>
+
+				<div className="flex flex-row shrink-0">
+					<p> carousel content </p>
+				</div>
+			</main>
 		</>
 	)
 }
